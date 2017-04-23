387643c8cd47184c83cef7909b27292548f8e25c
Victor Gaydov <victor@enise.org>
2017-04-23 17:29:25 +0300

    refactor classes (#23)

---
+++ lib/rdoc/generator/template_loader.rb
@@ -0,0 +1,48 @@
+require 'fileutils'
+require 'slim'
+require 'recursive-open-struct'
+
+require_relative 'settings'
+
+class TemplateLoader
+  def self.templates_dir
+    File.join Settings::data_dir, 'templates'
+  end
+
+  def self.templates_list
+    pattern = File.join self.templates_dir, '*.slim'
+
+    Dir[pattern].sort.map do |path|
+      File.basename path, '.slim'
+    end
+  end
+
+  def self.template_path(name)
+    if name.include? '/'
+      File.absolute_path name
+    else
+      File.join templates_dir, "#{name}.slim"
+    end
+  end
+
+  def load(path)
+    Template.new path
+  end
+end
+
+class Template
+  def initialize(path)
+    @path = path
+  end
+
+  def render(vars)
+    opts = {
+      pretty: true
+    }
+
+    scope = RecursiveOpenStruct.new(vars, recurse_over_arrays: true)
+
+    template = Slim::Template.new(@path, opts)
+    template.render(scope)
+  end
+end
--- lib/rdoc/generator/theme_loader.rb
+++ lib/rdoc/generator/theme_loader.rb
@@ -1,61 +1,57 @@
-class ThemeReader
-  DEFAULT_THEME    = 'light'.freeze
+require 'fileutils'
+require 'yaml'
+require 'sass'
 
-  def self.themes_dir(data_dir)
-    File.join data_dir, 'themes'
+require_relative 'settings'
+
+class ThemeLoader
+  def self.themes_dir
+    File.join Settings::data_dir, 'themes'
   end
 
-  def self.themes_list(data_dir)
-    Dir[File.join(themes_dir(data_dir), '*.yml')].sort.map do |path|
+  def self.themes_list
+    pattern = File.join themes_dir, '*.yml'
+
+    Dir[pattern].sort.map do |path|
       File.basename path, '.yml'
     end
   end
 
-  def self.theme_path(name, data_dir)
+  def self.theme_path(name)
     if name.include? '/'
       File.absolute_path name
     else
-      File.join self.themes_dir(data_dir), "#{name}.yml"
+      File.join themes_dir, "#{name}.yml"
     end
-  end
-
-  def self.build_url(name, prefix)
-    url = prefix || ''
-    url += '/' if !url.empty? && !url.end_with?('/')
-    url + name
   end
 
   def initialize(options)
     @options = options
   end
 
-  def read(default_theme_path)
+  def load(theme_paths)
     theme = {
       head: {
-        styles: [],
-        fonts: [],
+        styles:  [],
+        fonts:   [],
         scripts: [],
-        html: []
+        html:    []
       },
       body: {}
     }
 
-    theme_list = if @options.rsp_themes.empty?
-                   Array[default_theme_path]
-                 else
-                   @options.rsp_themes
-                 end
-
-    theme_list.each do |theme_path|
-      add_theme(theme, theme_path)
+    theme_paths.each do |theme_path|
+      load_theme(theme, theme_path)
     end
+
+    build_theme(theme)
 
     theme
   end
 
   private
 
-  def add_theme(theme, theme_path)
+  def load_theme(theme, theme_path)
     config = YAML.load_file theme_path
 
     config.each do |section, content|
@@ -75,14 +71,14 @@ class ThemeReader
           )
           section = key.to_sym
           files.each do |file_info|
-            path = theme_file_path(theme_path, file_info['file'])
+            path = theme_file(theme_path, file_info['file'])
             case section
             when :styles, :scripts, :fonts
               name = File.basename(path)
               file = {
                 src_path: path,
                 dst_name: name,
-                url:      ThemeReader.build_url(name, @options.rsp_prefix)
+                url:      theme_url(name)
               }
             when :html
               file = {
@@ -101,7 +97,7 @@ class ThemeReader
             expected: %w(header footer),
             actual:   key
           )
-          theme[:body][key.to_sym] = File.read(theme_file_path(theme_path, path))
+          theme[:body][key.to_sym] = File.read(theme_file(theme_path, path))
         end
       end
     end
@@ -109,8 +105,37 @@ class ThemeReader
     raise "Can't load theme - #{theme_path}\n#{error}"
   end
 
-  def theme_file_path(theme_path, file_path)
+  def build_theme(theme)
+    theme[:head][:styles].each do |file|
+      build_css_from_sass(file) if File.extname(file[:src_path]) == '.sass'
+    end
+  end
+
+  def build_css_from_sass(file)
+    options = {
+      cache:  false,
+      syntax: :sass,
+      style:  :default
+    }
+
+    input_data = File.read(file[:src_path])
+    renderer = Sass::Engine.new(input_data, options)
+    output_data = renderer.render
+
+    file.delete(:src_path)
+    file[:dst_name] = File.basename(file[:dst_name], '.*') + '.css'
+    file[:url] = theme_url(file[:dst_name])
+    file[:data] = output_data
+  end
+
+  def theme_file(theme_path, file_path)
     File.join(File.dirname(theme_path), file_path)
+  end
+
+  def theme_url(name)
+    url = @options.rsp_prefix || ''
+    url += '/' if !url.empty? && !url.end_with?('/')
+    url + name
   end
 
   def check_one_of(message: '', expected: [], actual: '')
